cff4f62cb03ef79a1853023d93fb2ec7c0193ca5
Rendiawan10 <147222618+Rendiawan10@users.noreply.github.com>
2023-10-11 18:29:11 +0800

    Add files via upload

---
+++ Day3percabangan.java
@@ -0,0 +1,15 @@
+package day.pkg3percabangan;
+public class Day3percabangan {
+    public static void main(String[] args) {
+        int umur = 20;
+    if (umur >= 17){
+            System.out.println("masih mudah");
+    } else {
+        System.out.println("sudah tua");
+    }
+        
+        
+        
+    }
+    
+}
