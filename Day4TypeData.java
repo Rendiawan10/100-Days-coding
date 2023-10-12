5bd6d3c563354fc16a008c067e31fe9e70b042e7
Rendiawan10 <147222618+Rendiawan10@users.noreply.github.com>
2023-10-12 18:07:55 +0800

    Tipe data

---
+++ Day4TypeData.java
@@ -0,0 +1,47 @@
+package day.pkg4.type.data;
+      //ini adalah contoh pemggunaan tipe data primitif dalam java.
+public class Day4TypeData {
+    public static void main(String[] args) {
+        // Tipe data byte digunakan untuk menyimpan bilangan bulat dalam rentang  -128 hingga 127.
+        byte byterendy = 42;
+        
+        // Tipe data short digunakan untuk menyimpan bilangan bulat dalam rentang -32,768 hingga 32,767.
+        short shortrendy = 1000;
+        
+        //Tipe data int digunakan untuk menyimpan bilangan bulat dalam rentang -2,147,483,648 hingga 2,147,483,647.
+        int intrendy = 100000;
+        
+        // Tipe data long digunakan untuk menyimpan bilangan bulat dalam rentang lebih besar.
+        long longrendy = 123456789L;
+        
+        // Tipe data float digunakan untuk menyimpan bilangan desimal dengan presisi lebih rendah.
+        float floatrendy = 3.14f;
+        
+        // Tipe data double digunakan untuk menyimpan bilangan desimal dengan presisi lebih tinggi
+        double doublerendy = 3.87548275647;
+        
+        // Tipe data char digunakan untuk menyimpan karakter tunggal.
+        char charrendy = 'R';
+        
+        // Tipe data boolean digunakan untuk menyimpan nilai benar atau salah.
+        boolean booleanrendy = true;
+        
+        // Tipe data non primitif
+        String Stringrendy ;
+            Stringrendy = "Rendiawan";
+        
+        // Tampilkan nilai dari masing-masing variabel
+        System.out.println("ini hasil tipe data byterendy: " + byterendy);
+        System.out.println("ini hasil tipe data shortrendy: " + shortrendy);
+        System.out.println("ini hasil tipe data intrendy: " + intrendy);
+        System.out.println("ini hasil tipe data longrendy: " + longrendy);
+        System.out.println("ini hasil tipe data floatrendy: " + floatrendy);
+        System.out.println("ini hasl tipe data doublerendy: " + doublerendy);
+        System.out.println("ini hasil tipe data charrendy: " + charrendy);
+        System.out.println("ini hasil tipedata booleanrendy: + booleanrendy");
+        System.out.println("ini hasil tipe data Stringrendy: " + Stringrendy);
+        
+        
+    }
+    
+}
