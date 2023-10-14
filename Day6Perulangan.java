f0265f0c8551379636022d6c4fca2f38631d598a
Rendiawan10 <147222618+Rendiawan10@users.noreply.github.com>
2023-10-14 17:34:04 +0800

    Perulangan

---
+++ Day6Perulangan.java
@@ -0,0 +1,10 @@
+package day.pkg6.perulangan;
+public class Day6Perulangan {
+    public static void main(String[] args) {
+        for (int R = 1; R <= 10; R++) {
+            System.out.println("R");
+        }
+        
+    }
+    
+}
