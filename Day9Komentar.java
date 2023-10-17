315515a572c0e652579ac1fde14f23667405667f
Rendiawan10 <147222618+Rendiawan10@users.noreply.github.com>
2023-10-17 20:38:35 +0800

    Komentar

---
+++ Day9Komentar.java
@@ -0,0 +1,19 @@
+package day.pkg9.komentar;
+public class Day9Komentar {
+    public static void main(String[] args) {
+        // Ini adalah komentar satu baris
+        
+        /*
+        Ini adalah contoh komentar
+        dalam bebeapa baris
+        */
+        
+        /**
+         * Ini adalah komentar dokumentasi.
+         * Biasanya di gunakan untuk menjelaskan fungsi atau kelas.
+         */
+        
+        System.out.println("jangan lupa, ngoding!"); // Komentar bisa digunakan di sebelah pernyataan kode juga
+    }
+    
+}
