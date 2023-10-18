d4adb10092bbca30e5e43d9c436b44cd64de1681
Rendiawan10 <147222618+Rendiawan10@users.noreply.github.com>
2023-10-18 21:46:40 +0800

    Mencetak/Menampilkan data

---
+++ Main.java
@@ -0,0 +1,19 @@
+package day10cetakdata;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // Data yang akan di cetak
+        String nama = "Rendiawan";
+        int umur    = 20;
+        double tinggi = 175.5;
+        
+        // Mencetak data 
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " +umur + "tahun");
+        System.out.println("Tinggi: " + tinggi + "cm");
+        
+    }
+    
+}
