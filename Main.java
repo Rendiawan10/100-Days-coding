5ec95b41b97fb140a030c0fbb06cec4a2541dc96
Rendiawan10 <147222618+Rendiawan10@users.noreply.github.com>
2023-10-27 21:20:58 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package day19perulangan2;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int tinggi = 5; // Tinggi bisa di ubah ubah 
+        int lebar = 7; // Begitupun dengan lebarrr
+        
+        for (int i = 1; i <= tinggi; i++) {
+            for (int j = 1; j <= lebar; j++) {
+                System.out.print("* ");
+            }
+        System.out.println();
+        }
+    }
+}
