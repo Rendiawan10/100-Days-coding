9a8cb59b19e49385bedb853dd073b6e4656037ad
Rendiawan10 <147222618+Rendiawan10@users.noreply.github.com>
2023-10-31 22:07:47 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package day24polasegitigaterbalik;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int tinggi = 5;
+        
+        for (int i = tinggi; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
