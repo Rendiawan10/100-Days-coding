1795abc00cd437991538464b4210722092631c9e
Rendiawan10 <147222618+Rendiawan10@users.noreply.github.com>
2023-11-02 16:50:12 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package day25Variabel;
+public class Main {
+    public static void main(String[] args) {
+        
+        // Mendeklarasikan Variabel dengan tipea data int,double,String,boolean
+        int angka = 10;
+        double desimal = 3.14;
+        String teks = "Apa, kabare"; 
+        boolean benar = true; 
+        
+        // Menampilkan Nilai Variabel
+        System.out.println("Nilai Angka: " + angka);
+        System.out.println("Nilai Desimal: " + desimal);
+        System.out.println("Nilai Teks: " + teks);
+        System.out.println("Nilai Benar: " + benar);
+    }// Besok apa lagi yah
+    
+}// Kehabisan ide sudah
