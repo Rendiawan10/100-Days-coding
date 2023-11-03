528a1fe6ca53add73b3155ff59222df1fa2c173e
Rendiawan10 <147222618+Rendiawan10@users.noreply.github.com>
2023-11-03 18:41:52 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package day26fungsii;
+
+public class Main {
+    // Fungsi sederhana yang mengembalikan nilai
+    public static int tambah (int angka1, int angka2) {
+        return angka1 + angka2;
+    }
+
+    // Fungsi tanpa nilai pengembalian
+    public static void cetakPesan(String pesan) {
+        System.out.println(pesan);
+    }
+
+    public static void main(String[] args) {
+        int hasilPenambahan = tambah(5, 7);
+        cetakPesan("Hasil penambahan: " + hasilPenambahan);
+    }
+    
+}
