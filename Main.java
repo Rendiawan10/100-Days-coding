8be37d34d008a3632b8d09b90969066c9aa28646
Rendiawan10 <147222618+Rendiawan10@users.noreply.github.com>
2023-11-04 20:48:33 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package day27fungsi;
+        // Ini adalah program Java sederhana tentang funsi
+public class Main {
+    // Ini adalah fungsi utama
+    public static void main(String[] args) {
+        // Memanggil fungsi sapa()
+        sapa("Rendiawan");
+    }
+
+    // Ini adalah fungsi sapa()
+    // Fungsi ini menerima satu parameter dan mencetak pesan sapaan
+    public static void sapa(String nama) {
+        System.out.println("Halo, " + nama + "! Selamat datang di kampus universitas Sulawesi Barat.");
+    }
+}
