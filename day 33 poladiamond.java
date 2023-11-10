ac827345c6226550bf7698babafd1c786cf45729
Rendiawan10 <147222618+Rendiawan10@users.noreply.github.com>
2023-11-10 10:59:11 +0800

    poladiamond.java

--- day 33 poladiamond.java
+++ day 33 poladiamond.java
@@ -3,7 +3,7 @@ package day33poladiamond;
 public class Main {
     
     public static void main(String[] args) {
-        int n = 5;
+        int n = 10;
         for (int i = 1; i <= n; i++) {
             for (int j = n; j > i; j--) {
                 System.out.print("  ");
