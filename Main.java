c7244814a66c0a3f34374e4432a3e96ff5b5eeec
Rendiawan10 <147222618+Rendiawan10@users.noreply.github.com>
2023-11-14 22:42:27 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package day37;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int angka1 = 10;
+        int angka2 = 5;
+
+        // Operator Perbandingan
+        System.out.println("Apakah angka1 sama dengan angka2? " + (angka1 == angka2));
+        System.out.println("Apakah angka1 tidak sama dengan angka2? " + (angka1 != angka2));
+        System.out.println("Apakah angka1 lebih besar dari angka2? " + (angka1 > angka2));
+        System.out.println("Apakah angka1 lebih kecil dari angka2? " + (angka1 < angka2));
+        System.out.println("Apakah angka1 lebih besar atau sama dengan angka2? " + (angka1 >= angka2));
+        System.out.println("Apakah angka1 lebih kecil atau sama dengan angka2? " + (angka1 <= angka2));
+    }
+    
+}
