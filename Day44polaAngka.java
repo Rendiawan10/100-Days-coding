b6532087b40ebc290a99f4b244a344b586c92e07
Rendiawan10 <147222618+Rendiawan10@users.noreply.github.com>
2023-11-22 00:30:16 +1000

    Add files via upload

---
+++ Day44polaAngka.java
@@ -0,0 +1,14 @@
+package com.mycompany.day44polaangka;
+public class Day44polaAngka {
+    public static void main(String[] args) {
+        int n = 6;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        
+    }
+}
