d026a330511c3ef4baaaa9d536a657d4582eb544
Rendiawan <147222618+Rendiawan10@users.noreply.github.com>
2023-12-21 20:32:18 +0800

    Add files via upload

---
+++ day74MenjumlahkanAngka.java
@@ -0,0 +1,23 @@
+package pkg100_days_of_coding;
+
+import java.util.Scanner;
+public class day74MenjumlahkanAngka {
+    public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan Batas : ");
+        int batas = in.nextInt();
+        int jumlah = 0;
+        for (int i = 1; i <= batas; i++) {
+            jumlah += i;
+            if (i == 1) {
+                
+            }else{
+                System.out.print("+");
+            }
+            System.out.print(i);
+            
+        }
+        System.out.println(" = "+jumlah);
+    }
+    
+}
