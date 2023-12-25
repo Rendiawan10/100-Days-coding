a7dd9886de927ba944f156d49bf5e3248d90acfc
Rendiawan <147222618+Rendiawan10@users.noreply.github.com>
2023-12-25 22:27:14 +0800

    Add files via upload

---
+++ day78OperasiLogika.java
@@ -0,0 +1,17 @@
+package pkg100_days_of_coding;
+public class day78OperasiLogika {
+    public static void main(String[] args) {
+        boolean nilai1 = true;
+        boolean nilai2 = false;
+        
+        boolean hasilAnd = nilai1 && nilai2;
+        System.out.println("Hasil AND: " + hasilAnd);
+        
+        boolean hasilOr = nilai1 || nilai2;
+        System.out.println("Hasil OR: " + hasilOr);
+        
+        boolean hasilNot = !nilai1;
+        System.out.println("Hasil NOT: " + hasilNot);
+    }
+    
+}
