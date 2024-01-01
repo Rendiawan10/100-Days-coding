a01fbc3be88641d57e418030c0af6fa4d47b39e2
Rendiawan <147222618+Rendiawan10@users.noreply.github.com>
2024-01-01 20:51:39 +0800

    Add files via upload

---
+++ day85GUI_Biodata.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg100_days_of_coding;
+
+/**
+ *
+ * @author DELL
+ */
+public class day85GUI_Biodata {
+    
+}
